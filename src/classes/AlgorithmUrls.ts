5e09c732d870f7a6137341d022eb2048cfd730e0
fksyuan <fksyuan@163.com>
2025-06-18 12:00:13 +0800

    update algorithm for support DVC mode

--- src/classes/AlgorithmUrls.ts
+++ src/classes/AlgorithmUrls.ts
@@ -19,12 +19,12 @@ export class AlgorithmUrls {
     if (res?.rc === 0) {
       let isInited = false;
       res.result.forEach((item: any) => {
-        let ws = new WebSocket(`wss://${item.algoProxyDomain}/algoproxyV2`);
+        let ws = new WebSocket(`wss://${item.algoProxyDomain}/algoproxyV3`);
         ws.onopen = async function () {
           if (!isInited) {
-            that.primusMpcUrl = `wss://${item.algorithmDomain}/algorithmV2`;
-            that.primusProxyUrl = `wss://${item.algorithmDomain}/algorithm-proxyV2`;
-            that.proxyUrl = `wss://${item.algoProxyDomain}/algoproxyV2`;
+            that.primusMpcUrl = `wss://${item.algorithmDomain}/algorithmV3`;
+            that.primusProxyUrl = `wss://${item.algorithmDomain}/algorithm-proxyV3`;
+            that.proxyUrl = `wss://${item.algoProxyDomain}/algoproxyV3`;
             isInited = true;
           }
           ws.close();
